c4aef54fb227ab2fe63ad0d416297dd340c36252
joaonunesss <joaom_braga@hotmail.com>
2024-08-12 14:43:12 +0100

    cpp07/ex02

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmarinho <jmarinho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 17:07:18 by jmarinho          #+#    #+#             */
+/*   Updated: 2024/08/12 14:27:57 by jmarinho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[36m"
+# define RESET "\033[37m"
+
+template <typename T>
+class Array {
+	public:
+		Array();
+		~Array();
+		Array(unsigned int n);
+		Array(Array &other);
+		Array &operator=(Array &other);
+
+		T &operator[](unsigned int index);
+
+		unsigned int size();
+
+		class Out_Of_Bounds : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+	
+	private:
+		T *array;
+		unsigned int array_size;
+};
+
+#endif
+
+template<typename T>
+Array<T>::Array() : array(new T[0]()), array_size(0) {
+	std::cout << GREEN << "Default Constructor has been called" << RESET << std::endl;
+}
+
+template<typename T>
+Array<T>::~Array() {
+	std::cout << RED << "Default Destructor has been called" << RESET << std::endl;
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n) : array(new T[n]()), array_size(n) {
+	std::cout << BLUE << "Default Param Constructor has been called" << RESET << std::endl;
+}
+
+template<typename T>
+Array<T>::Array(Array &other) : array(new T[other.n]()), array_size(other.n){
+	*this = other;
+	std::cout << YELLOW << "Default Copy Constructor has been called" << RESET << std::endl;
+}
+
+template<typename T>
+Array<T> &Array<T>::operator=(Array &other) {
+	if (this != &other)
+	{
+		delete this->array;
+		this->array = new T[other.array_size]();
+		this->array_size = other.array_size;
+		for (unsigned int i = 0; i < other.array_size; i++)
+			this->array[i] = other.array[i];
+	}
+	std::cout << BLUE << "Default Assignment Operator has been called" << RESET << std::endl;
+	return *this;
+}
+
+template<typename T>
+T& Array<T>::operator[](unsigned int index) {
+	if (index >= this->array_size || index < 0)
+		throw Out_Of_Bounds();
+	//std::cout << BLUE << "Default Subscript Operator has been called" << RESET << std::endl;
+	return this->array[index];
+}
+
+template<typename T>
+char const *Array<T>::Out_Of_Bounds::what() const throw() {
+	return "Index out of bounds";
+}
+
+template<typename T>
+unsigned int Array<T>::size() {
+	return array_size;
+}
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmarinho <jmarinho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 12:12:42 by jmarinho          #+#    #+#             */
+/*   Updated: 2024/08/12 14:42:54 by jmarinho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+int main()
+{
+	Array<std::string> c;
+	Array<int> a(1);
+	
+	std::cout << "c array size is " << c.size() << std::endl;
+
+
+	a[0] = 42;
+	std::cout << "a array value is " << a[0] << std::endl;
+	std::cout << "a array size is " << a.size() << std::endl;
+
+	Array<int> b;
+
+	b = a;
+	std::cout << "b array value b4 changes is " << b[0] << std::endl;
+
+	a[0] = 100;
+	b[0] = 21;
+	std::cout << "a array value after changes is " << a[0] << std::endl;
+	std::cout << "b array value after changes is " << b[0] << std::endl;
+		
+	Array<char> d(6);
+
+	d[0] = 'a';
+	d[1] = 'b';
+	d[2] = 'c';
+	d[3] = 'd';
+	d[4] = 'e';
+	d[5] = '\0';
+
+	for (int k = 0; k < 5; k++)
+		std::cout << "d[" << k << "] array value is " << d[k] << std::endl;
+	std::cout << "d array size is " << d.size() << std::endl;
+
+	try {
+        Array<int> count(5);
+ 
+        for (unsigned int i = 0; i < count.size(); i++) {
+            count[i] = i * 10;
+        }
+
+        for (unsigned int i = 0; i < count.size(); i++) {
+            std::cout << "Element at index " << i << ": " << count[i] << std::endl;
+        }
+
+        std::cout << "Attempting to access an out-of-bounds index..." << std::endl;
+        count[10] = 100;
+
+    } catch (const std::exception &e) {
+		
+        std::cerr << e.what() << std::endl;
+    }
+}
